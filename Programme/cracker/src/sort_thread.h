34fb49caf3dc5b87f4b1302e2a7d2586ce27f951
Amadeo DAVID <amadeo.david@student.uclouvain.be>
2019-05-08 13:04:47 +0200

    Adding function declarations.

--- Programme/cracker/src/sort_thread.h
+++ Programme/cracker/src/sort_thread.h
@@ -8,6 +8,9 @@
 #ifndef SRC_SORT_THREAD_H_
 #define SRC_SORT_THREAD_H_
 
-
+void * GetPassword(shared_data_t *sharedData, void *returnString);
+void * CountVowels(char Password[]);
+void * CountConsonants(char Password[]);
+void * SortPasswords(shared_data_t * sharedData);
 
 #endif /* SRC_SORT_THREAD_H_ */
