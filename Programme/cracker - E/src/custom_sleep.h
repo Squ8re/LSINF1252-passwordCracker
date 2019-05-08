1b67552602eb943bb597ed69cde5a89f16aa6df7
Eduardo Vannini <eduardo.vannini@student.uclouvain.be>
2019-05-08 09:23:16 +0200

    Cleaned up branch 'reversing' to prepare it to be merged to branch 'master'.

--- Programme/cracker - E/src/custom_sleep.h
+++
@@ -1,8 +0,0 @@
-// Fichier qui permet d'avoir une meme fonction de "delay" pour plusieurs OS
-
-#ifndef CUST_SLEEP_HEADER
-#define CUST_SLEEP_HEADER
-
-int custom_sleep(unsigned int delay_ms);
-
-#endif
--- Programme/cracker - E/src/reader_thread.h
+++
@@ -1,8 +0,0 @@
-// Fichier reprenant les fonctions relatives a la lecture des fichiers par le thread de lecture
-
-#ifndef READER_THREAD_HEADER
-#define READER_THREAD_HEADER
-
-void *read_files(void *reader_params);
-
-#endif
