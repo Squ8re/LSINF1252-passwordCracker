c6d7ae34dad42b8bf0fcd30fb748142b76e9dd95
Amadeo DAVID <amadeo.david@student.uclouvain.be>
2019-05-09 12:36:02 +0200

    Adding of the 'main.c' header.

---
+++ Programme/cracker/src/main.h
@@ -0,0 +1,13 @@
+/*
+ * main.h
+ *
+ *  Created on: 9 mai 2019
+ *      Author: amade
+ */
+
+#ifndef SRC_MAIN_H_
+#define SRC_MAIN_H_
+
+int main(int argc, char **argv);
+
+#endif /* SRC_MAIN_H_ */
